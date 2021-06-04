2124c6fdf8b69d0256e6efa6aba2c7f9171f3fdc
luizekstein <luiz_fekstein@hotmail.com>
2021-06-04 13:24:40 -0300

    Documentação + API

---
+++ Banco de Dados/Protótipo - Banco Individual.sql
@@ -0,0 +1,23 @@
+create database League_Of_Legends;
+
+use League_Of_Legends;
+
+create table Usuarios(
+	idUsuario int primary key not null auto_increment,
+    email varchar (60), 
+    nome varchar (80),
+    dia varchar (2),
+    mes varchar (10),
+    ano varchar (4),
+    login varchar (30),
+    senha varchar (50),
+    confirmarSenha varchar (50)
+);
+
+create table Personagens;
+
+select * from Usuarios;
+
+
+
+
---
+++ models/usuario.js
@@ -0,0 +1,65 @@
+	'use strict';
+
+/* 
+lista e explicação dos Datatypes:
+https://codewithhugo.com/sequelize-data-types-a-practical-guide/
+*/
+
+module.exports = (sequelize, DataTypes) => {
+    let Usuario = sequelize.define('Usuario',{
+		id: {
+			field: 'idUsuario',
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},	
+		email: {
+			field: 'email',
+			type: DataTypes.STRING,
+			allowNull: false
+		},	
+		nome: {
+			field: 'nome',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		dia: {
+			field: 'dia',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		mes: {
+			field: 'mes',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		ano: {
+			field: 'ano',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		login: {
+			field: 'login',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		senha: {
+			field: 'senha',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		confirmarSenha: {
+			field: 'confirmarSenha',
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+	}, 
+	{
+		tableName: 'Usuarios', 
+		freezeTableName: true, 
+		underscored: true,
+		timestamps: false,
+	});
+
+    return Usuario;
+};
